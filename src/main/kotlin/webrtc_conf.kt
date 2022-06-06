021d5504660081168eb59ec7a0ae06c4b04f2c49
TheSaminator <TheSaminator@users.noreply.github.com>
2022-06-06 17:26:13 -0400

    Change RTC STUN server

--- src/main/kotlin/webrtc_conf.kt
+++ src/main/kotlin/webrtc_conf.kt
@@ -1,28 +1,9 @@
 import externals.webrtc.RTCConfiguration
 import externals.webrtc.RTCIceServer
 import externals.webrtc.RTCPeerConnection
-import kotlinx.browser.window
-import kotlinx.coroutines.await
 
-suspend fun createRtcPeerConn() = if (isDevEnv)
-	RTCPeerConnection(getDevEnvRtcConfig())
-else
-	RTCPeerConnection(getRtcConfig())
+fun createRtcPeerConn() = RTCPeerConnection(getRtcConfig())
 
-fun getDevEnvRtcConfig() = configure<RTCConfiguration> {
-	val stunServer = configure<RTCIceServer> { urls = "stun:franciscusrex.dev" }
-	
-	iceServers = arrayOf(stunServer)
-}
-
-suspend fun getRtcConfig() = configure<RTCConfiguration> {
-	val stunServer = configure<RTCIceServer> { urls = "stun:franciscusrex.dev" }
-	val turnServer = window
-		.fetch("https://franciscusrex.dev/turn/confmaker.php?sitename=$GAME_NAME")
-		.await()
-		.json()
-		.await()
-		.unsafeCast<RTCIceServer>()
-	
-	iceServers = arrayOf(stunServer, turnServer)
+fun getRtcConfig() = configure<RTCConfiguration> {
+	iceServers = arrayOf(configure<RTCIceServer> { urls = "stun.l.google.com:19302" })
 }
